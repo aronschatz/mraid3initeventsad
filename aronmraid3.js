9cfa7de08a86967f132c90c646898686faec6101
aronschatz <aronschatz@aselabs.com>
2018-04-05 15:03:35 -0400

    .

--- aronmraid3.js
+++ aronmraid3.js
@@ -293,8 +293,9 @@ else
 function expandstatecheck()
 {
     //Add an event listener only for state change for this check
-    //mraid.addEventListener('stateChange',expandstatecheck);
-	mraid.expand();
+    mraid.addEventListener('stateChange',expandstatecheck);
+    mraid.expand();
+    console.log('called mraid.expand()');
 }
 
 function expandstatecheck()
